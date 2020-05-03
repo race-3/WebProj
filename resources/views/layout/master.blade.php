de4b060171228456a84ed517ce7161f0aa8b7a83
Tyler Race <tylerrrace@gmail.com>
2020-05-02 20:15:56 -0700

    Changed home page, added navbar and styling

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,2 +1,25 @@
-@yield('content')
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
+<style>
+    #title {
+        font-size: x-large;
+    }
+</style>
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#" id="title">Covid-19 Stats</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Tyler</a></li>
+            <li><a href="#">Alex</a></li>
+            <li><a href="#">Kareem</a></li>
+            <li><a href="#">Page 4</a></li>
+        </ul>
+    </div>
+</nav>
+
+@yield('content')
