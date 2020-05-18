11b8f5bfcf33706b227343ccacd9fb0a485ec4b4
aboueich <47013540+aboueich@users.noreply.github.com>
2020-05-18 13:00:16 -0700

    first chart

--- resources/views/pages/page3.blade.php
+++ resources/views/pages/page3.blade.php
@@ -15,12 +15,52 @@
         <!-- Styles -->
         <style>
             #title1 {
-                color: red;
+                color: black;
+                text-align: center;
+                padding: 10px;
+                border-bottom: 1px solid gray;
             }
+
+            [id*='item'] {
+                border-radius: 8px;
+                border: 1px solid black;
+                padding: 10px;
+                -webkit-box-shadow: 0 8px 6px -6px black;
+                -moz-box-shadow: 0 8px 6px -6px black;
+                box-shadow: 0 8px 6px -6px black;
+                margin-top: 20px;
+            }
+
         </style>
     </head>
     <body>
-        <h3 id="title1">TEST</h3>
-    </body>
+        <h3 id="title1">Economic Impacts of COVID-19</h3>
+
+        <div class="container" id="mainContainer">
+            <div class="row">
+                <div class="col-md-30">
+                    <div id="item1">
+                        <h3>Stuff</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit... stuff stuff stuff stuff stuff stuff more stuff stuff</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <h4>US Unemployment Rate, April 2019 - April 2020</h4>
+                    <iframe src='https://d3fy651gv2fhd3.cloudfront.net/embed/?s=usurtot&v=202005081237V20191105&d1=20190519&h=300&w=600' height='300' width='600'  frameborder='0' scrolling='no'></iframe>
+                    <br />
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <script type="text/javascript">
+
+
+
+
+        </script>
+
+
+            </body>
 </html>
 @endsection
