0d20afbbf1fe18d131a21226d941ea1ada0103af
aboueich <47013540+aboueich@users.noreply.github.com>
2020-05-21 10:48:37 -0700

    Added more charts

--- resources/views/pages/page3.blade.php
+++ resources/views/pages/page3.blade.php
@@ -34,6 +34,14 @@
                 font-weight: 700;
             }
 
+            #sectionTitle {
+                text-align: center;
+                padding-top: 10px;
+                padding-bottom: 5px;
+                font-weight: 700;
+                color: darkgray;
+            }
+
             #subtitle {
                 color: gray;
             }
@@ -77,6 +85,7 @@
                 </div>
             </div>
             <div class="row">
+                <h4 id="sectionTitle">Coronavirus Employment Effects</h4>
                 <div class="col-md-4">
                     <div id="item2">
                         <h4 id="headerTitle">US Unemployment Rate, April 2019 - April 2020</h4>
@@ -85,34 +94,58 @@
                         <h4 id="subtitle">Average Change Per Year</h4>
                         <p><b>Pre-Coronavirus</b>: 6.67% per month</p>
                         <p><b>During the pandemic</b>: 36% per month</p>
-                        <h4 id="footnote">Average is defined as the change month over month divided by the total number of months. This is focused on the <b>change</b>, not the direction of the change. For "During the pandemic", the greatest change is between March and April, where unemployment jumps over 330%</h4>
+                        <h4 id="footnote">Average is defined as the change month over month divided by the total number of months. This is focused on the <b>change</b>, not the direction of the change. For "During the pandemic", the greatest
+                            change is between March and April, where unemployment jumps over 330%</h4>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div id="item3">
-                        <h4 id="headerTitle">US Wage Growth, April 2019 - April 2020</h4>
+                        <h4 id="headerTitle">US Wage Growth, April 2019 - March 2020</h4>
                         <iframe src='https://tradingeconomics.com/embed/?s=unitedstawaggro&v=202004301856v20191105&h=300&w=600&ref=/united-states/wage-growth' height='300' width='100%'  frameborder='0' scrolling='no'></iframe>
                         <br />
+                        <h4 id="subtitle">Average Change Per Year</h4>
+                        <p><b>Pre-Coronavirus</b>: -.16% per month (about +4 growth per month overall)</p>
+                        <p><b>During the pandemic</b>: -2.265% per month</p>
+                        <h4 id="footnote">Before the pandemic, wages were growing between 4 and 5 percent consistently per month, estimated. While the growth has been gradually decreasing, the jump between Fedbruary and March, after the pandemic,
+                            is over 4 percent. Wages shrank for the first time for the first time since the Great Recession in 2008.</h4>
+                    </div>
+                </div>
+                <div class="col-md-4"
+                     <div id="item3">
+                         <h4 id="headerTitle">US Initial Jobless Claims, January 2020 - April 2020 (in thousands of claims)</h4>
+                         <iframe src='https://d3fy651gv2fhd3.cloudfront.net/embed/?s=ijcusa&v=202005211247V20191105&d1=20200101&d2=20200521&h=300&w=600' height='300' width='600'  frameborder='0' scrolling='no'></iframe>
+                         <br />
+                         <h4 id="subtitle">Average Change Per Week</h4>
+                         <p><b>Pre-Coronavirus (Average)</b>: 210,000</p>
+                         <p><b>During the pandemic (average)</b>: 2,046,000</p>
+                         <h4 id="footnote">The largest drops occurred in mid March, when two things happened: </h4>
+                         <h4 id="footnote">1. The stock market fell to its lowest level since 2016</h4>
+                         <h4 id="footnote">2. The US began approaching the start of the exponential growth in coronavirus cases</h4>
+                         <h4 id="footnote">It is difficult to determine if we were on path to a recession prior to the coronavirus. However, the job losses began to increase significantly when lockdowns began. While they have been decreasing
+                            over the course of the last 2 months, it is still very high and could go up further if there is a second wave. Whether everyone who lost their job will get it back wgen the pandemic is over is something that still
+                            remains to be seen.</h4>
+                     </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div id="item5">
+                        <h4 id="headerTitle">nationL DEBT</h4>
+
+                        <iframe src='https://tradingeconomics.com/embed/?s=unitedstagovdeb&v=202005091013v20191105&h=300&w=600&ref=/united-states/government-debt' height='300' width='100%'  frameborder='0' scrolling='no'></iframe>
+                        <br />
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div id="item3">
-                        <h4 id="headerTitle">US Consumer Spending, January 2017 - April 2020</h4>
+                    <div id="item4">
+                        <h4 id="headerTitle">US Consumer Spending, January 2017 - April 2020 (in millions of dollars)</h4>
                         <iframe src='https://d3fy651gv2fhd3.cloudfront.net/embed/?s=unitedstaconspe&v=202004291429V20191105&type=type=line&h=300&w=600' height='300' width='100%'  frameborder='0' scrolling='no'></iframe>
                         <br />
+                        <h4 id="footnote">Consumer spending (both for essential goods and non essential goods) makes up 70% of the US economy (Per Marketplace). It is often a good indicator of how the economy is expected to grow. It has been rising
+                            consistently the last few years, but up to January of this year, it takes a sharp downturn. This is related to the unemployment rate - people are not getting paid, so they can't spend money. </h4>
                     </div>
                 </div>
             </div>
         </div>
-
-        <script type="text/javascript">
-
-
-
-
-        </script>
-
-
-            </body>
+        </body>
 </html>
 @endsection
