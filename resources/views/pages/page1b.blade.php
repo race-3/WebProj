9c09bd8dc154172c0dab88851ed42fe1ceaf425b
Tyler Race <tylerrrace@gmail.com>
2020-05-23 14:28:48 -0700

    added 3rd line, and changed states up a bit

--- resources/views/pages/page1b.blade.php
+++ resources/views/pages/page1b.blade.php
@@ -45,9 +45,8 @@
                     <ul class="dropdown-menu">
                         <li><a href="#">Washington</a></li>
                         <li><a href="#">Oregon</a></li>
-                        <li><a href="#">Idaho</a></li>
+                        <li><a href="#">Missouri</a></li>
                         <li><a href="#">New York</a></li>
-                        <li><a href="#">California</a></li>
                     </ul>
                 </div>
                 <h2 id="loader" style="display: none">LOADING DATA FROM API...</h2>
@@ -88,13 +87,15 @@
 </div>
 
 <script type="text/javascript">
+    let co2_2017_url = 'https://aqs.epa.gov/data/api/sampleData/byState?email=tylerrrace@gmail.com&key=greenmouse56&param=45201&bdate=20170101&edate=20171230&state=';
     let co2_2018_url = 'https://aqs.epa.gov/data/api/sampleData/byState?email=tylerrrace@gmail.com&key=greenmouse56&param=45201&bdate=20180101&edate=20181230&state=';
     let co2_2019_url = 'https://aqs.epa.gov/data/api/sampleData/byState?email=tylerrrace@gmail.com&key=greenmouse56&param=45201&bdate=20190101&edate=20191230&state=';
-    let state = '', built_url1, built_url2;
+    let state, built_url1, built_url2, built_url3;
 
     function selectState(state) {
         built_url1 = co2_2018_url + state;
         built_url2 = co2_2019_url + state;
+        built_url3 = co2_2017_url + state;
         createChart1();
     }
 
@@ -109,14 +110,11 @@
             case 'Oregon':
                 selectState(41);
                 break;
-            case 'Idaho':
-                selectState(16);
+            case 'Missouri':
+                selectState(29);
                 break;
             case 'New York':
                 selectState(36);
-                break;
-            case 'California':
-                selectState('06');
                 break;
             default:
                 break;
@@ -135,23 +133,34 @@
             data: {
                 labels: data.xs,
                 datasets: [{
-                    label: 'AQI Index in ' + state,
-                    data: data.ys,
+                    label: '2017 AQI Index in ' + state,
+                    data: data.ys3,
                     fill: false,
-                    backgroundColor: 'rgba(140, 140, 140, 0.2)',
-                    borderColor: 'rgba(0, 0, 0, 1)',
+                    borderColor: 'rgba(234, 152, 0, 1)',
                     borderWidth: 1
                 },
                 {
-                    label: 'AQI Index in ' + state,
+                    label: '2018 AQI Index in ' + state,
+                    data: data.ys,
+                    fill: false,
+                    borderColor: 'rgba(234, 0, 3, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: '2019 AQI Index in ' + state,
                     data: data.ys2,
                     fill: false,
-                    backgroundColor: 'rgba(0, 200, 140, 0.2)',
-                    borderColor: 'rgba(0, 0, 0, 1)',
+                    borderColor: 'rgba(0, 24, 209, 1)',
                     borderWidth: 1
-                }]
+                }
+                ]
             },
             options: {
+                elements: {
+                    point:{
+                        radius: 0
+                    }
+                },
                 scales: {
                     yAxes: [{
                         scaleLabel: {
@@ -160,6 +169,12 @@
                         }
                     }],
                     xAxes: [{
+                        ticks: {
+                            callback: function(value, index, values) {
+                                let arr = value.split("-");
+                                    return arr[1] + "-" + arr[2];
+                            }
+                        },
                         scaleLabel: {
                             display: true,
                             labelString: 'Date of sample'
@@ -174,12 +189,16 @@
         const ys = [];
         const xs2 =[];
         const ys2 = [];
+        const xs3 =[];
+        const ys3 = [];
 
         document.getElementById('loader').style.display = 'block';
         const response = await fetch(built_url1);
         const response2 = await fetch(built_url2);
+        const response3 = await fetch(built_url3);
         const data = await response.json();
         const data2 = await response2.json();
+        const data3 = await response3.json();
         document.getElementById('loader').style.display = 'none';
         data['Data'].forEach(obj => {
             /*
@@ -199,8 +218,17 @@
             xs2.push(obj['date_local']);
             ys2.push(parseFloat(obj['sample_measurement']));
         });
+        data3['Data'].forEach(obj => {
+            /*
+            Object.entries(obj).forEach(([key, value]) => {
+                console.log(`${key} ${value}`);
+            });*/
 
-        return {xs, ys, xs2, ys2};
+            xs3.push(obj['date_local']);
+            ys3.push(parseFloat(obj['sample_measurement']));
+        });
+
+        return {xs, ys, xs2, ys2, xs3, ys3};
     }
 
     /*async function createChart2() {
