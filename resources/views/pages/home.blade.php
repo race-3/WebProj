210ae2ac4104e3fc111296f3c101f71f219ba8a6
aboueich <47013540+aboueich@users.noreply.github.com>
2020-06-02 15:04:16 -0700

    slight front page cleanup work

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -13,9 +13,12 @@
             text-align: center;
             font-size: 40pt;
         }
+
         #description {
             text-align: center;
             font-size: 16pt;
+            font-weight: 500;
+            border: none;
         }
     </style>
 
@@ -29,20 +32,24 @@
 <div class="container">
     <div class="col-s-12" id="description">
         This is a website dedicated to exploring some of the effects COVID-19 has had on various aspects of life.
-        Instead of infection rates, recovery rates, or death rates, we explore other effects and statistics,
+        Instead of looking at infection rates, recovery rates, or death rates, we explore other effects and statistics,
         such as changes in the stock market or air quality.
     </div>
 
     <div class="col-s-12" id="description">
-        <br>For more information on COVID-19:
+        <br>For more information on COVID-19
+        <br>Heath Organizations:
         <br><a href="https://www.cdc.gov/coronavirus/2019-nCoV/index.html">CDC</a>
         <br><a href="https://govstatus.egov.com/OR-OHA-COVID-19">Oregon Health Authority</a>
+        <br><br>News Updates:
         <br><a href="https://www.google.com/search?q=coronavirus&rlz=1C1SQJL_enUS790US790&source
             =lnms&tbm=nws&sa=X&ved=2ahUKEwigqvbvzcXpAhWJpZ4KHbdzDbIQ_AUoAXoECCIQAw&biw=1707&bih=838&dpr=1.13">News Updates</a>
-        <br><a href="https://sharedsystems.dhsoha.state.or.us/DHSForms/Served/le2279a.pdf">Information on Testing (Local)</a>
-        <br><a href="https://www.google.com/maps/search/coronavirus+testing/@45.4991224,-122.7832994,12z">Testing Sites in the Portland area</a>
         <br><a href="https://www.google.com/search?q=coronavirus+vaccine&rlz=1C1SQJL_enUS790US790&source=lnms&tbm=
             nws&sa=X&ved=2ahUKEwjCm_3N9ePpAhXI7Z4KHVX-BgQQ_AUoAXoECAwQAw&biw=1707&bih=838&dpr=1.13">Vaccine Updates</a>
+        <br><br>Testing Information:
+        <br><a href="https://sharedsystems.dhsoha.state.or.us/DHSForms/Served/le2279a.pdf">Information on Testing (Local)</a>
+        <br><a href="https://www.google.com/maps/search/coronavirus+testing/@45.4991224,-122.7832994,12z">Testing Sites in the Portland area</a>
+        <br><br>Statistics:
         <br><a href="https://www.worldometers.info/coronavirus/">Statistics (Worldwide)</a>
         <br><a href="https://www.worldometers.info/coronavirus/country/us/">Statistics (USA)</a>
     </div>
