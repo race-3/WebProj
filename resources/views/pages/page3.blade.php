bb34a25438b36382fdf3dd9f9be8d27fac8620d7
aboueich <47013540+aboueich@users.noreply.github.com>
2020-06-02 12:36:59 -0700

    jump to pages menu v1

--- resources/views/pages/page3.blade.php
+++ resources/views/pages/page3.blade.php
@@ -125,7 +125,7 @@
                 left: 0;
                 right: 0;
                 bottom: 0;
-                background-color: #ccc;
+                background-color: #F1F1F1;
                 -webkit-transition: .4s;
                 transition: .4s;
             }
@@ -237,6 +237,53 @@
                 font-size: 18px;
             }
 
+            .menubtn {
+                background-color: #DC143C;
+                color: lightgray;
+                padding: 15px;
+                border: none;
+                font-size: 15px;
+                font-weight: 700;
+                left: 50%;
+                margin-right: -50%;
+                display: inline-block;
+            }
+
+            .jumpToMenu {
+                position: relative;
+                display: inline-block;
+                left: 50%;
+                margin-right: -50%;
+            }
+
+            .sections {
+                display: none;
+                position: absolute;
+                background-color: #999999;
+                z-index: 2;
+                min-width: 300px;
+            }
+
+            .sections a {
+                color: black;
+                padding: 10px 15px;
+                display: block;
+            }
+
+            .sections a:hover {
+                background-color: #999999;
+            }
+
+            .jumpToMenu:hover
+            .sections {
+                display: block;
+            }
+
+            .jumpToMenu:hover
+            .menubtn {
+                background-color: #DC143C;
+            }
+
         </style>
     </head>
 
@@ -250,13 +297,22 @@
         </label>
         </h4>
 
-        <div id="jumpTo">
+        <!-- <div id="jumpTo">
             <h5>Jump to: </h5>
             <a href="#COVIDEmployment"><b>Coronavirus Employment Data</b></a>
             <br>
             <a href="#COVIDGovSP"><b>Coronavirus Government Spending</b></a>
             <br>
             <a href="#COVIDConsumerRE"><b>Coronavirus Consumer Effects</b></a>
+        </div> -->
+
+        <div class="jumpToMenu">
+            <button class="menubtn">Jump to:</button>
+            <div class="sections">
+                <a href="#COVIDEmployment"><b>Coronavirus Employment Data</b></a>
+                <a href="#COVIDGovSP"><b>Coronavirus Government Spending</b></a>
+                <a href="#COVIDConsumerRE"><b>Coronavirus Consumer Effects</b></a>
+            </div>
         </div>
 
         <button onclick="toTop()" id="topbtn">Top</button>
